29f580c1bf81f7971561f89910ec4dbb8df3daca
Parag-G <parag@infrrd.ai>
2017-08-08 10:48:13 +0530

    Initial code

---
+++ recruitmentmanagement/src/main/java/com/infrrd/recruitmentmanagement/RecruitmentmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.infrrd.recruitmentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentmanagementApplication.class, args);
+	}
+}
